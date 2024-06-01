1606140d3d053f053ce6f12d6d382b551ccb3441
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-06-01 18:55:59 +0100

    added more responsive for forecast

--- app/components/ForecastCard.tsx
+++ app/components/ForecastCard.tsx
@@ -44,14 +44,15 @@ export default function ForecastCard({
 
   return (
     <div className="flex items-center">
-      <h2 className="w-1/3 pl-10 text-gray-400">{day}</h2>
-      <div className="flex w-1/3 items-center content-center ">
-        <Image src={image} alt="weather" height={100} width={100} />
-        <p>{weatherText}</p>
+      <h2 className="w-1/3 pl-5 text-gray-400">{day}</h2>
+      <div className="flex flex-col text-gray-400 w-1/3 items-center text-center">
+        <Image src={image} alt="weather" height={80} width={80} />
+        <p className="mx-auto text-sm">{weatherText}</p>
       </div>
-      <p className="w-1/3 pl-20">
-        {Math.floor(maxTemp)} / {Math.floor(minTemp)}
-      </p>
+      <div className="w-1/3 text-right pr-5 text-sm md:text-lg">
+        <p>{Math.floor(maxTemp)}</p>
+        <p>{Math.floor(minTemp)}</p>
+      </div>
     </div>
   );
 }
